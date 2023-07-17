6b7726c55f64bcc6fa75023c765d62de340a8b0f
Kevin Delgado <kevindel891@gmail.com>
2023-07-16 22:04:50 -0700

    Initialize React Project

---
+++ my-app/src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  /*
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to cringe.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  */
+
+
+  return (
+    <div className='App'>
+      <header>
+        <nav>
+          <ul>
+            <li>About Me</li>
+            <li>My Work</li>
+            <li>Resume</li>
+            <li>Contact Me!</li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
